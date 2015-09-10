314d368b73adc34dc5bc9824e8a1759524f7a4e8
Firas Sabbah <firasazz@gmail.com>
2015-09-10 19:09:49 +0200

    Modify test_get_winner in rows, columns, left and right diagonal, change loops to ruby-style loops

--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -21,7 +21,7 @@ class ConnectFourTest < ConnectFourSpec
 
   def test_add_movement_to_full_column
     col = 1
-    for i in 0..7
+    8.times do
       @game_board.add_movement @playerx, col
     end
     last_movement = @game_board.add_movement @playerx, col
@@ -29,8 +29,8 @@ class ConnectFourTest < ConnectFourSpec
   end
 
   def test_is_full
-    for i in 0..7
-      for j in 0..7
+    8.times do
+      8.times do |j|
         @game_board.add_movement @playerx, j
       end
     end
@@ -38,25 +38,25 @@ class ConnectFourTest < ConnectFourSpec
   end
 
   def test_get_winner_in_rows
-    for i in 0..3
+    4.times do |i|
       @game_board.add_movement @playerx, i
     end
-    assert_equal @playerx, @game_board.get_winner
+    assert_equal @playerx, @game_board.get_winner_in_rows
   end
 
   def test_get_winner_in_columns
-    for i in 0..3
+    4.times do
       @game_board.add_movement @playerx, 0
     end
-    assert_equal @playerx, @game_board.get_winner
+    assert_equal @playerx, @game_board.get_winner_in_columns
   end
 
-  def test_get_winner_in_left_diagonal
-    for i in 0..2
+  def test_get_winner_in_right_diagonal
+    3.times do
       @game_board.add_movement @playero, 0
     end
     @game_board.add_movement @playerx, 0
-    for i in 0..1
+    2.times do
       @game_board.add_movement @playero, 1
     end
     @game_board.add_movement @playerx, 1
@@ -64,15 +64,15 @@ class ConnectFourTest < ConnectFourSpec
     @game_board.add_movement @playero, 2
     @game_board.add_movement @playerx, 2
     @game_board.add_movement @playerx, 3
-    assert_equal @playerx, @game_board.get_winner
+    assert_equal @playerx, @game_board.get_winner_in_right_diagonal
   end
 
-  def test_get_winner_in_right_diagonal
-    for i in 0..2
+  def test_get_winner_in_left_diagonal
+    3.times do
       @game_board.add_movement @playero, 7
     end
     @game_board.add_movement @playerx, 7
-    for i in 0..1
+    2.times do
       @game_board.add_movement @playero, 6
     end
     @game_board.add_movement @playerx, 6
@@ -80,7 +80,7 @@ class ConnectFourTest < ConnectFourSpec
     @game_board.add_movement @playero, 5
     @game_board.add_movement @playerx, 5
     @game_board.add_movement @playerx, 4
-    assert_equal @playerx, @game_board.get_winner
+    assert_equal @playerx, @game_board.get_winner_in_left_diagonal
   end
 
 end
